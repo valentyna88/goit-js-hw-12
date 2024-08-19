4a1cda3faf1d977faa6a8428b35261f714ca5e40
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-19 18:26:04 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,24 @@
 import axios from 'axios';
 
-export async function fetchImages(query, currentPage) {
-  axios.defaults.baseURL = 'https://pixabay.com/api/';
-  const params = new URLSearchParams({
-    key: '45452240-d9bf2a206a145f9e2645b735d',
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 15,
-    page: currentPage,
-  });
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '45452240-d9bf2a206a145f9e2645b735d';
 
-  const response = await axios.get('', { params });
-  return response.data;
+export async function fetchImages(query, currentPage) {
+  try {
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page: currentPage,
+    });
+
+    const response = await axios.get('', { params });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
 }
