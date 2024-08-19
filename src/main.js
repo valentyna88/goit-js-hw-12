e3353612776b512ea1976ce27a275bf544838e1b
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-19 19:10:16 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@ import { fetchImages } from './js/pixabay-api';
 import { renderImages } from './js/render-functions';
 
 const searchFormEl = document.querySelector('.js-search-form');
+const gallery = document.querySelector('.js-gallery');
+const loadMoreBtnEl = document.querySelector('.load-more-btn');
 const loader = document.querySelector('.loader');
+
 let lightbox;
+let query = '';
+let currentPage = 1;
 
-searchFormEl.addEventListener('submit', async function (event) {
+searchFormEl.addEventListener('submit', onSearch);
+loadMoreBtnEl.addEventListener('click', onLoadMore);
+
+async function onSearch(event) {
   event.preventDefault();
-
   const query = event.target.elements.user_query.value.trim();
 
   if (query === '') {
@@ -19,10 +26,14 @@ searchFormEl.addEventListener('submit', async function (event) {
     });
     return;
   }
+  currentPage = 1;
+  gallery.innerHTML = '';
 
+  loadMoreBtnEl.classList.add('is-hidden');
   loader.classList.remove('is-hidden');
+
   try {
-    const data = await fetchImages(query);
+    const data = await fetchImages(query, currentPage);
     if (data.hits.length === 0) {
       iziToast.error({
         title: 'No Results',
@@ -36,9 +47,11 @@ searchFormEl.addEventListener('submit', async function (event) {
       } else {
         lightbox = new SimpleLightbox('.gallery a');
       }
+      if (data.totalHits > currentPage * 15) {
+        loadMoreBtnEl.classList.remove('is-hidden');
+      }
     }
   } catch (error) {
-    console.error('Error fetching images:', error);
     iziToast.error({
       title: 'Error',
       message: 'Failed to fetch images. Please try again later.',
@@ -47,4 +60,4 @@ searchFormEl.addEventListener('submit', async function (event) {
     loader.classList.add('is-hidden');
     event.target.reset();
   }
-});
+}
