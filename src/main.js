2b95ec73637213364ea6ffafe9219118e052cabe
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-20 13:24:23 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,20 +4,13 @@ import { fetchImages } from './js/pixabay-api';
 import { renderImages } from './js/render-functions';
 
 const searchFormEl = document.querySelector('.js-search-form');
-const galleryEl = document.querySelector('.js-gallery');
-const loadMoreBtnEl = document.querySelector('.load-more-btn');
 const loader = document.querySelector('.loader');
-
 let lightbox;
-let query = '';
-let currentPage = 1;
 
-searchFormEl.addEventListener('submit', onSearch);
-loadMoreBtnEl.addEventListener('click', onLoadMore);
-
-async function onSearch(event) {
+searchFormEl.addEventListener('submit', function (event) {
   event.preventDefault();
-  query = event.target.elements.user_query.value.trim();
+
+  const query = event.target.elements.user_query.value.trim();
 
   if (query === '') {
     iziToast.error({
@@ -27,68 +20,34 @@ async function onSearch(event) {
     return;
   }
 
-  currentPage = 1;
-  galleryEl.innerHTML = '';
-  loadMoreBtnEl.classList.add('is-hidden');
   loader.classList.remove('is-hidden');
 
-  try {
-    const data = await fetchImages(query, currentPage);
-    if (data.hits.length === 0) {
-      iziToast.error({
-        title: 'No Results',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-      });
-    } else {
-      renderImages(data.hits);
-      if (lightbox) {
-        lightbox.refresh();
+  fetchImages(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: 'No Results',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
       } else {
-        lightbox = new SimpleLightbox('.gallery a');
+        renderImages(data.hits);
+        if (lightbox) {
+          lightbox.refresh();
+        } else {
+          lightbox = new SimpleLightbox('.gallery a');
+        }
       }
-
-      if (data.totalHits > 15) {
-        loadMoreBtnEl.classList.remove('is-hidden');
-      }
-    }
-  } catch (error) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Failed to fetch images. Please try again later.',
-    });
-  } finally {
-    loader.classList.add('is-hidden');
-    event.target.reset();
-  }
-}
-
-async function onLoadMore() {
-  currentPage += 1;
-  loader.classList.remove('is-hidden');
-  loadMoreBtnEl.classList.add('is-hidden');
-
-  try {
-    const data = await fetchImages(query, currentPage);
-    renderImages(data.hits);
-    lightbox.refresh();
-
-    const totalLoadedImages = currentPage * 15;
-    if (totalLoadedImages >= data.totalHits) {
-      loadMoreBtnEl.classList.add('is-hidden');
-      iziToast.info({
-        title: 'End of Results',
-        message: "We're sorry, but you've reached the end of search results.",
+    })
+    .catch(error => {
+      console.error(error);
+      iziToast.error({
+        title: 'Error',
+        message: 'Failed to fetch images. Please try again later.',
       });
-    } else {
-      loadMoreBtnEl.classList.remove('is-hidden');
-    }
-  } catch (error) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Failed to fetch images. Please try again later.',
+    })
+    .finally(() => {
+      loader.classList.add('is-hidden');
+      event.target.reset();
     });
-  } finally {
-    loader.classList.add('is-hidden');
-  }
-}
+});
