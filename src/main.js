c1d90a72a3406f4e614f964918962c1c976fe7b8
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-22 13:49:13 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,35 +7,36 @@ const searchFormEl = document.querySelector('.js-search-form');
 const loader = document.querySelector('.loader');
 let lightbox;
 
-searchFormEl.addEventListener('submit', function (event) {
+const onSearchFormSubmit = event => {
   event.preventDefault();
 
-  const query = event.target.elements.user_query.value.trim();
+  const searchedValue = searchFormEl.elements.user_query.value.trim();
 
-  if (query === '') {
+  if (searchedValue === '') {
     iziToast.error({
       title: 'Error',
       message: 'Please enter a search term.',
+      position: 'topRight',
     });
     return;
   }
 
   loader.classList.remove('is-hidden');
 
-  fetchImages(query)
+  fetchImages(searchedValue)
     .then(data => {
       if (data.hits.length === 0) {
         iziToast.error({
-          title: 'No Results',
           message:
             'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
         });
       } else {
         renderImages(data.hits);
         if (lightbox) {
           lightbox.refresh();
         } else {
-          lightbox = new SimpleLightbox('.gallery a');
+          lightbox = new SimpleLightbox('.js-gallery a');
         }
       }
     })
@@ -48,6 +49,7 @@ searchFormEl.addEventListener('submit', function (event) {
     })
     .finally(() => {
       loader.classList.add('is-hidden');
-      event.target.reset();
+      searchFormEl.reset();
     });
-});
+};
+searchFormEl.addEventListener('submit', onSearchFormSubmit);
