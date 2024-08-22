888ee9205490038f708e606ed831c99334c67dd8
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-22 17:29:22 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,18 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export const fetchImages = searchedQuery => {
-  const params = new URLSearchParams({
-    q: searchedQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 15,
-    key: '45452240-d9bf2a206a145f9e2645b735d',
-  });
+  const axiosOptions = {
+    params: {
+      q: searchedQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      key: '45452240-d9bf2a206a145f9e2645b735d',
+    },
+  };
 
-  return fetch(`${BASE_URL}?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios.get(``, axiosOptions);
 };
