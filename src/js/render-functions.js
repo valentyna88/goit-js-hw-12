df93301bee54712a9afecf8e9f7aa898f15d87cd
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-08-25 18:06:52 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,43 +1,25 @@
-export const renderImages = images => {
-  return images
-    .map(
-      ({
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
-    <div class="image-container">
-      <li class="gallery-item">
-        <div class="image-viewer">
-          <a class="gallery-link" href="${largeImageURL}">
-            <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-          </a>
-        </div>
-        <ul class="gallery-info-list">
-          <li class="gallery-info-item">
-            <h3>Likes</h3>
-            <p>${likes}</p>
-          </li>
-          <li class="gallery-info-item">
-            <h3>Views</h3>
-            <p>${views}</p>
-          </li>
-          <li class="gallery-info-item">
-            <h3>Comments</h3>
-            <p>${comments}</p>
-          </li>
-          <li class="gallery-info-item">
-            <h3>Downloads</h3>
-            <p>${downloads}</p>
-          </li>
-        </ul>
-      </li>
-    </div>
-  `
-    )
-    .join('');
-};
+export const createGalleryCardTemplate = ({
+  largeImageURL,
+  webformatURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) => `
+  <div class="image-container">
+    <li class="gallery-item">
+      <div class="image-viewer">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+      </div>
+      <ul class="gallery-info-list">
+        <li class="gallery-info-item"><h3>Likes</h3><p>${likes}</p></li>
+        <li class="gallery-info-item"><h3>Views</h3><p>${views}</p></li>
+        <li class="gallery-info-item"><h3>Comments</h3><p>${comments}</p></li>
+        <li class="gallery-info-item"><h3>Downloads</h3><p>${downloads}</p></li>
+      </ul>
+    </li>
+  </div>
+`;
